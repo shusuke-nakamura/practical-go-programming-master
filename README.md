86e0015e1b7b96c4c78b8925079cf363000e2ae2
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-09-06 19:42:30 +0900

    webserviceの学習まで完了

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # practical-go-programming-master
+
+# app_a/webservice
+go get github.com/rs/zerolog
---
+++ app_a/func/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// def-func
+func calc(x, y int) int {
+	return x + y
+}
+
+// def-func
+
+// def-func2
+func calcAge(y int, m time.Month, d int) (age int, err error) {
+	b := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
+	n := time.Now()
+	if b.After(n) {
+		err = errors.New("誕生日が未来です")
+		return
+	}
+	for {
+		b = time.Date(y+age+1, m, d, 0, 0, 0, 0, time.Local)
+		if b.After(n) {
+			return
+		}
+		age++
+	}
+}
+
+// def-func2
+
+// def-func4
+func doCalc(x, y int, f func(int, int) int) {
+	fmt.Println()
+}
+
+// def-func4
+
+func main() {
+	fmt.Println(calcAge(1980, time.November, 14))
+	m := func(x, y int) int {
+		return x * y
+	}
+
+	doCalc(10, 20, m)
+	doCalc(10, 10, func(x, y int) int {
+		return x * y
+	})
+
+	fmt.Println(m(1, 2))
+
+	f, err := os.Create("sample.txt")
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+
+	defer f.Close()
+	io.WriteString(f, "hello world")
+
+}
